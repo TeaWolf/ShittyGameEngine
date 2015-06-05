bee4b7c8c7b2502fe20d4f9d462d6691781b16d3
TeaWolf <TeaWolf@users.noreply.github.com>
2015-06-05 22:03:29 +0200

    ...

--- game_object.h
+++ game_object.h
@@ -3,16 +3,26 @@
 #pragma once
 
 #include <SDL2/SDL.h>
+#include "texture_manager.h"
+
 class GameObject
 {
 public:
-	GameObject() = default;
-	virtual ~GameObject() = default;	
+	GameObject(int x, int y, int width, int height, const std::string& texture_id);
+	virtual ~GameObject();
 	
-	virtual void draw();
 	virtual void update();
+	virtual void draw(SDL_Renderer* renderer);
 
 protected:
 	int m_x;
 	int m_y;
+
+	int m_width;
+	int m_height;
+
+	std::string m_texture_id;
+
+	int m_current_row;
+	int m_current_frame;
 };
