6997d68713078c6ae28f55f587ab9199860cad9e
TeaWolf <TeaWolf@users.noreply.github.com>
2015-06-06 23:20:03 +0200

    Adds the blueprint for GameObjects from end chapter 3

--- game_object.h
+++ game_object.h
@@ -3,25 +3,17 @@
 #pragma once
 
 #include <SDL2/SDL.h>
+
 #include "texture_manager.h"
+#include "object_load_parameters.h"
 
 class GameObject
 {
 public:
-	virtual void init(int x, int y, int width, int height, const std::string& texture_id);	
-	virtual void update();
-	virtual void draw(SDL_Renderer* renderer);
-	virtual void clean();
+	GameObject(const ObjectLoadParameters* lparams) {}
+	virtual ~GameObject() {}
 
-protected:
-	int m_x;
-	int m_y;
-
-	int m_width;
-	int m_height;
-
-	std::string m_texture_id;
-
-	int m_current_row;
-	int m_current_frame;
+	virtual void update() = 0;
+	virtual void draw() = 0;
+	virtual void clean() = 0;
 };
