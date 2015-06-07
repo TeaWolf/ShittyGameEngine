f4331b0cde6c023412171d9cfbe6d84446a1f94d
TeaWolf <TeaWolf@users.noreply.github.com>
2015-06-07 21:10:31 +0200

    Changes ObjectLoadParameters to accept velocity and accel

--- object_load_parameters.cpp
+++ object_load_parameters.cpp
@@ -2,6 +2,16 @@
 
 #include "object_load_parameters.h"
 
-ObjectLoadParameters::ObjectLoadParameters(int p_x, int p_y, int p_width, int p_height, const std::string& p_texture_id)
-: x(p_x), y(p_y), width(p_width), height(p_height), texture_id(p_texture_id)
+ObjectLoadParameters::ObjectLoadParameters(
+	int p_x, int p_y,
+	double p_vel_x, double p_vel_y,
+	double p_acc_x, double p_acc_y,
+	int p_width, int p_height,
+	const std::string& p_texture_id)
+:
+	x(p_x), y(p_y),
+	vel_x{p_vel_x}, vel_y{p_vel_y},
+	acc_x{p_acc_x}, acc_y{p_acc_y},
+	width(p_width), height(p_height),
+	texture_id(p_texture_id)
 {}
--- object_load_parameters.h
+++ object_load_parameters.h
@@ -7,9 +7,16 @@
 class ObjectLoadParameters
 {
 public:
-	ObjectLoadParameters(int p_x, int p_y, int p_width, int p_height, const std::string& p_texture_id);
-	~ObjectLoadParameters() {}
+	ObjectLoadParameters(
+		int p_x, int p_y,
+		double p_vel_x, double p_vel_y,
+		double p_acc_x, double p_acc_y,
+		int p_width, int p_height,
+		const std::string& p_texture_id
+	);
+	virtual ~ObjectLoadParameters() = default;
 
 	int x, y, width, height;
+	double vel_x, vel_y, acc_x, acc_y;
 	std::string texture_id;
 };
