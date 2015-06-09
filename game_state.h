cf58d5a0e76d2bbf06db4e25ac812fa956dc173a
TeaWolf <TeaWolf@users.noreply.github.com>
2015-06-09 21:59:02 +0200

    First version of GameState base class

---
+++ game_state.h
@@ -0,0 +1,17 @@
+// Teawolf
+
+#pragma once
+
+#include <string>
+
+class GameState
+{
+public:
+	virtual void update() = 0;
+	virtual void render() = 0;
+
+	virtual bool on_enter() = 0;
+	virtual bool on_exit() = 0;
+
+	virtual std::string get_state_id() const = 0;
+};
