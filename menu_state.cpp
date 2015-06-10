a6b102025e10627212dc3c5f0ae1913d26c2922a
TeaWolf <TeaWolf@users.noreply.github.com>
2015-06-10 21:49:24 +0200

    A functionning menu state

--- menu_state.cpp
+++ menu_state.cpp
@@ -5,19 +5,47 @@
 const std::string MenuState::s_menu_id = "MENU";
 
 void MenuState::update()
-{}
+{
+	for (auto* go : m_game_objects)
+		go->update(); 
+}
 
 void MenuState::render()
-{}
+{
+	for (auto* go : m_game_objects)
+		go->draw();
+}
 
 bool MenuState::on_enter()
 {
 	std::clog << "Entering " << s_menu_id << " state" << std::endl;
+
+	// Load the button's image
+	if (!TextureManager::instance()->load("../assets/blue-button.png", "blue_button", Game::instance()->get_renderer()))
+		return false;
+
+	// Load the buttons objects
+	// TODO fuck that fucking parameter object
+	ObjectLoadParameters* lparams = new ObjectLoadParameters(0, 0, 0, 0, 0, 0, 255, 49, "blue_button");
+	m_game_objects.push_back(new MenuButton(lparams));
+	lparams->y += 150;
+	m_game_objects.push_back(new MenuButton(lparams));
+	delete lparams;
+
 	return true;
 }
 
 bool MenuState::on_exit()
 {
 	std::clog << "Exiting " << s_menu_id << " state" << std::endl;
+
+	// Clear all game object ressources
+	for (auto* go : m_game_objects)
+		go->clean();
+	m_game_objects.clear();
+
+	// Clear all loaded textures
+	TextureManager::instance()->clear_texture("blue_button");
+
 	return true;
 }
--- menu_state.h
+++ menu_state.h
@@ -5,10 +5,14 @@
 #include <iostream>
 
 #include "game_state.h"
+#include "game.h"
+#include "menu_button.h"
 
 class MenuState : public GameState
 {
 public:
+	~MenuState() = default;
+
 	void update();
 	void render();
 
@@ -17,7 +21,9 @@ public:
 
 	std::string get_state_id() const {return s_menu_id;}
 
-private:
+protected:
 	
 	static const std::string s_menu_id;
+
+	std::vector<GameObject*> m_game_objects;
 };
